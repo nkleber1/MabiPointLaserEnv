bfc229729a98b1beee0ccafe32fcc4f7d9fe4259
nkleber <nils.kleber@freenet.de>
2021-11-14 18:53:17 +0100

    generate new datasets

--- point_clouds/show_data.py
+++ point_clouds/show_data.py
@@ -1,8 +1,8 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
-data = np.load('../meshes/train_data/point_clouds/lidar.npy')
-for i in range(79, 100):
+data = np.load('../meshes/train_data/point_clouds/uniform_density_2048.npy')
+for i in range(100):
     plt.scatter(data[i, :, 0], data[i, :, 1])
     print(i)
     plt.show()
--- point_clouds/uniform_point_cloud_generator.py
+++ point_clouds/uniform_point_cloud_generator.py
@@ -2,27 +2,42 @@ from pyntcloud import PyntCloud
 import open3d as o3d
 import matplotlib.pyplot as plt
 import numpy as np
+import os
 
-mesh_file = 'C:/Users/nilsk/Projects/MabiPointLaserEnv/meshes/train_data/meshes/r_map9.stl'
+num_meshes = len(os.listdir('C:/Users/nilsk/Projects/MabiPointLaserEnv/meshes/train_data/meshes/'))
+num_positions = 10
+num_points = 2048
 
-triangle_mesh = o3d.io.read_triangle_mesh(mesh_file)
+def load_room(index):
+    # load a room (code form mesh.py Mesh.load_stl(file))
+    mesh_file = 'C:/Users/nilsk/Projects/MabiPointLaserEnv/meshes/train_data/meshes/r_map{}.stl'.format(index)
+    triangle_mesh = o3d.io.read_triangle_mesh(mesh_file)
+    room = PyntCloud.from_instance("open3d", triangle_mesh)
+    return room
 
-room = PyntCloud.from_instance("open3d", triangle_mesh)
-# room.plot(mesh=True, backend="matplotlib")
-room = room.get_sample("mesh_random", n=10000, rgb=False, normals=False, as_PyntCloud=True)
-# room.plot(backend="matplotlib")
-df = room.points
-max_z = df['z'].max()
-df = df[df['z'] > 0]
-df = df[df['z'] < max_z]
-
-pts = df.to_numpy()[:, :2]
-
-pts = pts[np.random.choice(pts.shape[0], 5000, replace=False)]
-print(pts.shape)
-
-plt.scatter(pts[:, 0], pts[:, 1])
-plt.show()
+# START SCRIPT
+dataset = None
+for i in range(1, num_meshes+1):
+    room = load_room(i)
+    room = room.get_sample("mesh_random", n=num_points*4, rgb=False, normals=False, as_PyntCloud=True)
+    # room.plot(backend="matplotlib")
+    df = room.points
+    max_z = df['z'].max()
+    df = df[df['z'] > 0]
+    df = df[df['z'] < max_z]
+    pts = df.to_numpy()[:, :2]
+    pts = pts[np.random.choice(pts.shape[0], num_points, replace=False)]
+    # plt.scatter(pts[:, 0], pts[:, 1])
+    # plt.show()
+    point_cloud = pts - pts.min()
+    pts = pts / pts.max()
+    pts = np.expand_dims(pts, 0)
+    if dataset is None:
+        dataset = pts
+    else:
+        dataset = np.concatenate([dataset, pts], axis=0)
+print(dataset.shape)
+np.save('../meshes/train_data/point_clouds/uniform_density_2048.npy', dataset)
 
 
 
