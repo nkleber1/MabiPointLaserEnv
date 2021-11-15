8065b065af274a96f2127ce850c83faa790a28c6
nkleber <nils.kleber@freenet.de>
2021-11-15 21:10:44 +0100

    rename class

--- point_clouds/embedding/foldingnet/__init__.py
+++ point_clouds/embedding/foldingnet/__init__.py
@@ -1,3 +1,3 @@
-from .model_fold_decoder import Fold_Decoder
+from .model_fold_decoder import FoldDecoder
 from .model_graph_encoder import GraphEncoder
 from .model_pointnet2_encoder import PointNet2Encoder
