bbc04b825eb3c97f62d53c44c9047eabceb79783
nkleber <nils.kleber@freenet.de>
2021-11-16 11:03:49 +0100

    generate bigger uniform_density datasets

--- point_clouds/embedding/point_cloud_generators/uniform_point_cloud_generator.py
+++ point_clouds/embedding/point_cloud_generators/uniform_point_cloud_generator.py
@@ -4,9 +4,9 @@ import matplotlib.pyplot as plt
 import numpy as np
 import os
 
-num_meshes = len(os.listdir('/meshes/train_data/meshes/'))
+num_meshes = len(os.listdir('C:/Users/nilsk/Projects/MabiPointLaserEnv/meshes/train_data/meshes/'))
 num_positions = 10
-num_points = 2048
+num_points = 512
 
 def load_room(index):
     # load a room (code form mesh.py Mesh.load_stl(file))
@@ -15,29 +15,38 @@ def load_room(index):
     room = PyntCloud.from_instance("open3d", triangle_mesh)
     return room
 
+def rotate_pointcloud(pointcloud):
+    theta = np.pi * 2 * np.random.choice(24) / 24
+    rotation_matrix = np.array([[np.cos(theta), -np.sin(theta)], [np.sin(theta), np.cos(theta)]])
+    pointcloud[:, [0,1]] = pointcloud[:, [0,1]].dot(rotation_matrix)  # random rotation (x,z)
+    return pointcloud
+
 # START SCRIPT
 dataset = None
 for i in range(1, num_meshes+1):
     room = load_room(i)
     room = room.get_sample("mesh_random", n=num_points*4, rgb=False, normals=False, as_PyntCloud=True)
     # room.plot(backend="matplotlib")
-    df = room.points
-    max_z = df['z'].max()
-    df = df[df['z'] > 0]
-    df = df[df['z'] < max_z]
-    pts = df.to_numpy()[:, :2]
-    pts = pts[np.random.choice(pts.shape[0], num_points, replace=False)]
-    # plt.scatter(pts[:, 0], pts[:, 1])
-    # plt.show()
-    point_cloud = pts - pts.min()
-    pts = pts / pts.max()
-    pts = np.expand_dims(pts, 0)
-    if dataset is None:
-        dataset = pts
-    else:
-        dataset = np.concatenate([dataset, pts], axis=0)
+    for num in range(num_positions):
+        df = room.points
+        max_z = df['z'].max()
+        df = df[df['z'] > 0]
+        df = df[df['z'] < max_z]
+        pts = df.to_numpy()[:, :2]
+        pts = pts[np.random.choice(pts.shape[0], num_points, replace=False)]
+        # plt.scatter(pts[:, 0], pts[:, 1])
+        # plt.show()
+        if num is not 0:
+            point_cloud = rotate_pointcloud(point_cloud)
+        point_cloud = pts - pts.min()
+        pts = pts / pts.max()
+        pts = np.expand_dims(pts, 0)
+        if dataset is None:
+            dataset = pts
+        else:
+            dataset = np.concatenate([dataset, pts], axis=0)
 print(dataset.shape)
-np.save('../../../meshes/train_data/point_clouds/uniform_density_2048.npy', dataset)
+np.save('../../../meshes/train_data/point_clouds/uniform_density_512_big.npy', dataset)
 
 
 
